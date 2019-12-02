c3d2afd9ebda310357f1bfa26a369fe8e1a00b6a
arsy12 <henihestuningsih@gmail.com>
2019-12-02 13:20:10 +0700

    heni

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -77,8 +77,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div id="body">
 		<p>Halaman ini dibuat oleh framework CodeIgniter.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<p>Heni Hestuningsih</code>
 
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
